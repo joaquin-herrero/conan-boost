71f712edb85ddc71ac55c2ba40b2ac181d137a0c
Joaquín Herrero Herrero <jherrero.sax@gmail.com>
2019-03-26 17:54:09 +0100

    Fixed missing import on build.py

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 from cpt.packager import ConanMultiPackager
-
+import os
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(options={"boost:python_version": 3.7, "boost:python_executable": os.environ['Python_EXECUTABLE'], "boost:python_include_dir": os.environ['Python_INCLUDE_DIR'], "boost:python_lib_dir": os.environ['Python_LIBRARY_RELEASE']})
+    builder = ConanMultiPackager(options={"boost:python_version": 3.7, "boost:python_executable": os.environ['Python_EXECUTABLE'], "boost:python_include_dir": os.environ['Python_INCLUDE_DIR'], "boost:python_library": os.environ['Python_LIBRARY_RELEASE']})
     builder.add_common_builds()
     builder.run()

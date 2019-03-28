b0dde75951d9da6ed577735d34f6178a639f6dd5
Joaquín Herrero Herrero <jherrero.sax@gmail.com>
2019-03-28 17:59:05 +0100

    Fixed travis conan commands.

--- .travis/run.sh
+++ .travis/run.sh
@@ -10,4 +10,10 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
+conan profile new --detect default
+conan profile update env.Python_EXECUTABLE=$Python_EXECUTABLE default
+conan profile update env.Python_INCLUDE_DIR=$Python_INCLUDE_DIR default
+conan profile update env.Python_LIBRARY_RELEASE=$Python_LIBRARY_RELEASE default
+conan profile update options.boost:python_version=3.7 default
+
 python build.py

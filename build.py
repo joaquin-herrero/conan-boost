feceb8875566a2358d26a206509c96cb77d58460
Joaquín Herrero Herrero <jherrero.sax@gmail.com>
2019-03-28 14:12:57 +0100

    Modified build.py to show env variables.

--- build.py
+++ build.py
@@ -2,6 +2,8 @@ from cpt.packager import ConanMultiPackager
 import os
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(options={"boost:python_version": 3.7, "boost:python_executable": os.environ['Python_EXECUTABLE'], "boost:python_include_dir": os.environ['Python_INCLUDE_DIR'], "boost:python_library": os.environ['Python_LIBRARY_RELEASE']})
+    print(os.environ)
+    builder = ConanMultiPackager(options={"boost:python_version": "3.7"},
+      env_vars={"Python_EXECUTABLE": os.environ['Python_EXECUTABLE'], "Python_INCLUDE_DIR": os.environ['Python_INCLUDE_DIR'], "Python_LIBRARY_RELEASE": os.environ['Python_LIBRARY_RELEASE']})
     builder.add_common_builds()
     builder.run()

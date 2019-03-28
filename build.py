83d3a2c7342669c151ee00fc9ed9e76ebbab75b2
Joaquín Herrero Herrero <jherrero.sax@gmail.com>
2019-03-28 15:10:58 +0100

    Modified conan profile to set env variables instead of CPT.

--- build.py
+++ build.py
@@ -2,8 +2,6 @@ from cpt.packager import ConanMultiPackager
 import os
 
 if __name__ == "__main__":
-    print(os.environ)
-    builder = ConanMultiPackager(options={"boost:python_version": "3.7"},
-      env_vars={"Python_EXECUTABLE": os.environ['Python_EXECUTABLE'], "Python_INCLUDE_DIR": os.environ['Python_INCLUDE_DIR'], "Python_LIBRARY_RELEASE": os.environ['Python_LIBRARY_RELEASE']})
+    builder = ConanMultiPackager(options={"boost:python_version": "3.7"})
     builder.add_common_builds()
     builder.run()
